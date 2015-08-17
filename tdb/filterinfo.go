d6538ba6db2deb6bf4ed2ca3e74da6202cbb5ba4
Toomore Chiang <toomore0929@gmail.com>
2015-08-17 11:17:29 +0000

    Add tdb/filterinfo

---
+++ tdb/filterinfo.go
@@ -0,0 +1,30 @@
+package tdb
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+)
+
+type filterinfo base
+
+var insertFilterinfoSQL *sql.Stmt
+
+func NewFilterinfoDB() *filterinfo {
+	table := "filterinfo"
+	if insertFilterinfoSQL, err = conn.Prepare(fmt.Sprintf("Insert into %s(no, description) Values(?,?) ON DUPLICATE KEY UPDATE no=?, description=?", table)); err != nil {
+		log.Fatal(err)
+	}
+
+	return &filterinfo{
+		table: table,
+	}
+}
+
+func (filterinfo) InsertFilterinfo(no int64, desc string) (sql.Result, error) {
+	return insertFilterinfoSQL.Exec(no, desc, no, desc)
+}
+
+func (filterinfo) Close() error {
+	return insertFilterinfoSQL.Close()
+}
